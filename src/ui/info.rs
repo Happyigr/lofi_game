3d3bf58f9f8d01721c6c18cfaec33e8fa37f4fcc
happyigr <mihakuwork@gmail.com>
2024-08-16 10:07:41 +0200

    colored ui for enemies

--- src/ui/info.rs
+++ src/ui/info.rs
@@ -1,14 +1,41 @@
 use crate::enemy::SuperPower;
-use bevy_egui::{egui, EguiContexts};
+use bevy::color::ColorToPacked;
+use bevy_egui::{
+    egui::{self, Color32},
+    EguiContexts,
+};
 use strum::IntoEnumIterator;
 
 pub fn spawn_info_window(mut contexts: EguiContexts) {
     egui::Window::new("Info").show(contexts.ctx_mut(), |ui| {
-        ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
-            ui.label("Type | Kill Key");
-            for superpower in SuperPower::iter() {
-                ui.label(format!("{} | {}", superpower, superpower.get_keycode_str()));
-            }
-        });
+        ui.with_layout(
+            egui::Layout {
+                main_dir: egui::Direction::TopDown,
+                main_wrap: false,
+                main_align: egui::Align::Center,
+                main_justify: true,
+                cross_align: egui::Align::Center,
+                cross_justify: false,
+            },
+            |ui| {
+                ui.horizontal_top(|ui| {
+                    ui.label("Type");
+                    ui.label(" | ");
+                    ui.label("Kill Key");
+                });
+
+                for superpower in SuperPower::iter() {
+                    ui.horizontal_top(|ui| {
+                        let color = superpower.get_enemy_color().to_linear().to_u8_array();
+                        ui.colored_label(
+                            Color32::from_rgba_unmultiplied(color[0], color[1], color[2], color[3]),
+                            format!("{superpower}"),
+                        );
+                        ui.label(" | ");
+                        ui.label(format!("{}", superpower.get_keycode_str()));
+                    });
+                }
+            },
+        );
     });
 }
